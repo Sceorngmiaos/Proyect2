268c886d6d880e16675da0377794f74774d9d16a
Sergio Cabello <91514692+Sceorngmiaos@users.noreply.github.com>
2021-11-04 10:06:05 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+const form = document.getElementById("form")
 const ApiKey = document.getElementById("Apikey")
 const radio = document.getElementById("rad")
 const perimetro = document.getElementById("per")
@@ -5,3 +6,7 @@ const area = document.getElementById("are")
 const textura = document.getElementById("tex")
 const suavidad = document.getElementById("sua")
 
+form.addEventListener("submit",e=>{
+
+}
+                      
