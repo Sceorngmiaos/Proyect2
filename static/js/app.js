7f540c5d8b9ed2ea2649605e1cac92623476b05a
Sergio Cabello <91514692+Sceorngmiaos@users.noreply.github.com>
2021-11-04 09:53:33 -0600

    Create app.js

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+const ApiKey = document.getElementById("Apikey")
+const radio = document.getElementById("rad")
+const perimetro = document.getElementById("per")
+const area = document.getElementById("are")
+const textura = document.getElementById("tex")
+const suavidad = document.getElementById("sua")
+const radio = document.getElementById("rad")
