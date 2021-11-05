58770078b532a3c7cdc8948605dcfaa627083f17
Sergio Cabello <91514692+Sceorngmiaos@users.noreply.github.com>
2021-11-05 00:01:19 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,15 @@
-const form = document.getElementById("form")
-const ApiKey = document.getElementById("Apikey")
-const radio = document.getElementById("rad")
-const perimetro = document.getElementById("per")
-const area = document.getElementById("are")
-const textura = document.getElementById("tex")
-const suavidad = document.getElementById("sua")
+const form = document.getElementById("myForm");
+const ApiKey = document.getElementById("Apikey");
+const radio = document.getElementById("rad");
+const perimetro = document.getElementById("per");
+const area = document.getElementById("are");
+const textura = document.getElementById("tex");
+const suavidad = document.getElementById("sua");
 
-form.addEventListener("submit",e=>{
-	e.preventDefault()
-	let warnings = ''
-	warnings = "Hola"
+form.addEventListener("submit", (e)=>{
+	e.preventDefault();
+	console.log("Form has been submitted:")
 	
-})
+});
                       
 
-function getToken(errorCallback, loadCallback) {
-	const req = new XMLHttpRequest();
-	req.addEventListener("load", loadCallback);
-	req.addEventListener("error", errorCallback);
-	req.open("POST", "https://iam.cloud.ibm.com/identity/token");
-	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	req.setRequestHeader("Accept", "application/json");
-	req.send("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" + API_KEY);
-}
