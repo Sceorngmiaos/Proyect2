3a3b4a9bdd9b9b1ac0c54021d05080afd917e73a
Sergio Cabello <91514692+Sceorngmiaos@users.noreply.github.com>
2021-11-05 00:06:32 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,21 @@
 const form = document.getElementById("myForm");
-const ApiKey = document.getElementById("Apikey");
+const API_KEY = document.getElementById("Apikey");
 const radio = document.getElementById("rad");
 const perimetro = document.getElementById("per");
 const area = document.getElementById("are");
 const textura = document.getElementById("tex");
 const suavidad = document.getElementById("sua");
+                     
+
+function getToken(errorCallback, loadCallback) {
+	const req = new XMLHttpRequest();
+	req.addEventListener("load", loadCallback);
+	req.addEventListener("error", errorCallback);
+	req.open("POST", "https://iam.cloud.ibm.com/identity/token");
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	req.setRequestHeader("Accept", "application/json");
+	req.send("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" + API_KEY);
+};
 
 form.addEventListener("submit", (e)=>{
 	e.preventDefault();
@@ -18,14 +29,3 @@ form.addEventListener("submit", (e)=>{
 	}
 	
 });
-                      
-
-function getToken(errorCallback, loadCallback) {
-	const req = new XMLHttpRequest();
-	req.addEventListener("load", loadCallback);
-	req.addEventListener("error", errorCallback);
-	req.open("POST", "https://iam.cloud.ibm.com/identity/token");
-	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	req.setRequestHeader("Accept", "application/json");
-	req.send("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" + API_KEY);
-}
