2b0cf156fe5f1c770b89cf74fd00b7e1ad559d8b
Sergio Cabello <91514692+Sceorngmiaos@users.noreply.github.com>
2021-11-05 00:05:38 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -9,7 +9,23 @@ const suavidad = document.getElementById("sua");
 form.addEventListener("submit", (e)=>{
 	e.preventDefault();
 	console.log("Form has been submitted:")
+	getToken((err) => console.log(err), function () {
+	let tokenResponse;
+	try {
+		tokenResponse = JSON.parse(this.responseText);
+	} catch(ex) {
+		// TODO: handle parsing exception
+	}
 	
 });
                       
 
+function getToken(errorCallback, loadCallback) {
+	const req = new XMLHttpRequest();
+	req.addEventListener("load", loadCallback);
+	req.addEventListener("error", errorCallback);
+	req.open("POST", "https://iam.cloud.ibm.com/identity/token");
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	req.setRequestHeader("Accept", "application/json");
+	req.send("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" + API_KEY);
+}
